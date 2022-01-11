0add41e794b6c797743a3f9fcb30467f755cbac4
CodeX-fi <68636441+CodeX-fi@users.noreply.github.com>
2022-01-11 11:24:07 +0200

    Visual improvements

---
+++ Opus.Styles/Converters/ZeroAndTrue.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace Opus.Styles.Converters
+{
+    public class ZeroAndTrue : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (values[0] is int count && values[1] is bool selected)
+            {
+                if (selected == false)
+                    return Visibility.Collapsed;
+                if (count > 0)
+                    return Visibility.Collapsed;
+                return Visibility.Visible;
+            }
+
+            return Visibility.Collapsed;
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
